1865b13ebd870ce055e984a84e84d4362f71f973
vukstojanovic <vuks838@gmail.com>
2021-10-06 19:51:48 +0200

    everything except localstorage

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -6,19 +6,25 @@ function App() {
 
   const [name, setName] = useState("");
   const [groceries, setGroceries] = useState([]);
-  const [alerted, setAlerted] = useState(false);
+  const [alerted, setAlerted] = useState({show: false, message: "", type: ""});
   const [isEdit, setIsEdit] = useState(false);
+  const [itemID, setItemID] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    if (isEdit) {
-      setGroceries(...groceries);
-    }
-
-    if (name) {
+    if (isEdit && name) {
+      setGroceries(groceries.map((item, index) => {
+        if (index === itemID) {
+          return name;
+        }
+        return item;
+      }));
+      setAlerted({show: true, message: "Item edited", type: "success"});
+      setIsEdit(false);
+    } else if (name) {
       setGroceries([...groceries, name]);
-      setAlerted(true);
+      setAlerted({show: true, message: "Item added", type: "success"});
     }
 
     setName("");
@@ -28,37 +34,34 @@ function App() {
     const newList = [...groceries];
     newList.splice(i, 1);
     setGroceries(newList);
+    setAlerted({show: true, message: "Item deleted", type: "danger"});
   }
 
   function editItem(i) {
     setIsEdit(true);
-    const newList = groceries;
-    setName(newList[i]);
+    setItemID(i);
+    setName(groceries[i]);
   }
 
   function clearList() {
     setGroceries([]);
+    setAlerted({show: true, message: "List deleted", type: "danger"});
   }
 
-  useEffect(() => {
-    const ticking = setTimeout(() => {
-      setAlerted(false);
-    }, 2000)
-    return () => {
-      clearTimeout(ticking);
-    }
-  }, [groceries]);
+  function hideAlert() {
+    setAlerted({show: false, message: "", type: ""});
+  }
 
   // console.log(groceries);
 
   return (
     <section className="section-center">
       <form action="" className="grocery-form" onSubmit={handleSubmit}>
-        { alerted ? <Alert /> : null }
+        { alerted.show && <Alert hideAlert={hideAlert} {...alerted} groceries={groceries} /> }
         <h3>grocery bud</h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e. g. eggs" value={name} onChange={(e) => setName(e.target.value)} />
-          <button className="submit-btn">submit</button>
+          <button className="submit-btn">{isEdit ? "Edit": "Submit"}</button>
         </div>
       </form>
       <List groceries={groceries} deleteItem={deleteItem} clearList={clearList} editItem={editItem} />
