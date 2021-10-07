7a55211bf7cfc2a924fe7fd5235d9a6074230446
vukstojanovic <vuks838@gmail.com>
2021-10-07 17:05:13 +0200

    navbar done without useRef

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,40 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+
+  console.log(social);
+  const [burgerActive, setBurgerActive] = useState(false);
+
+  function toggleBurger() {
+    setBurgerActive(prevValue => !prevValue);
+  }
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="" className="logo"/>
+          <button className="nav-toggle"onClick={toggleBurger}>
+            <FaBars/>
+          </button>
+        </div>
+        <div className="links-container" style={burgerActive ? {height: "200px"} : {height: "0px"}}>
+          <ul className="links">
+            {links.map(link => {
+              const {id, url, text} = link;
+              return <li key={id}><a href={url}>{text}</a></li>
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map(s => {
+              const {id, url, icon} = s;
+              return <li key={id}><a href={url}>{icon}</a></li>
+            })}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
