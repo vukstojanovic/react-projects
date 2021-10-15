bd45a885c9200fd6e24feb16ffdda1b97c2f6ee2
vukstojanovic <vuks838@gmail.com>
2021-10-15 09:59:15 +0200

    dhsdfg

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,24 +5,35 @@ import logo from './logo.svg'
 
 const Navbar = () => {
 
-  console.log(social);
   const [burgerActive, setBurgerActive] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
   function toggleBurger() {
     setBurgerActive(prevValue => !prevValue);
   }
+
+  useEffect(() => {
+    console.log(linksRef.current.getBoundingClientRect().height);
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (burgerActive) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [burgerActive]);
 
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="" className="logo"/>
-          <button className="nav-toggle"onClick={toggleBurger}>
+          <button className="nav-toggle" onClick={toggleBurger}>
             <FaBars/>
           </button>
         </div>
-        <div className="links-container" style={burgerActive ? {height: "200px"} : {height: "0px"}}>
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map(link => {
               const {id, url, text} = link;
               return <li key={id}><a href={url}>{text}</a></li>
